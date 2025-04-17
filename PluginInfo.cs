f6afdb070faec5e77484747e965379a8ed300969
Liexxxx <jsamalot30@gmail.com>
2025-04-17 04:39:55 -0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod.gg
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.gorillatag.mods";
+        public const string Name = "Mod.gg";
+        public const string Description = "Menu open to the public";
+        public const string Version = "1.0.2";
+    }
+}
